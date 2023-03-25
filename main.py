2893106066593f2e9b32b8e5d2dc3a6c80d1eaa8
Dikshant Gupta <guptadikshant99@gmail.com>
2023-03-25 20:10:48 +0530

    added logic to retrieve single post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+from random import randrange
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -14,19 +14,39 @@ class Post(BaseModel):
     rating: Optional[int] = None
 
 
+my_posts = [
+    {"title": "first title", "content": "content of first title", "id": 1},
+    {"title": "second title", "content": "content of second title", "id": 2},
+]
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
+
 @app.get('/')
 def root():
     return {"messages": "Hello World!"}
 
 
-@app.get('/post')
+@app.get('/posts')
 def get_posts():
-    return {"data": "this is your post"}
+    return {"data": my_posts}
 
 
-@app.post('/createposts')
+@app.post('/posts')
 def create_post(post: Post):
-    print(post.dict())
+    post_dict = post.dict()
+    post_dict["id"] = randrange(0, 10000000)
+    my_posts.append(post.dict())
     return {
-        "data": post
+        "data": post_dict
     }
+
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post = find_post(id)
+    return {"post": post}

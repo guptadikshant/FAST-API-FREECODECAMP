4c98c71664a0421d85aad7d8c53c7dd7de459372
Dikshant Gupta <guptadikshant99@gmail.com>
2023-03-25 20:23:48 +0530

    raise HTTP Exception if id is not available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from random import randrange
-from fastapi import FastAPI
+from fastapi import FastAPI, Response,status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 
@@ -47,6 +47,9 @@ def create_post(post: Post):
 
 
 @app.get("/posts/{id}")
-def get_post(id:int):
+def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"No post found for id {id}")
     return {"post": post}

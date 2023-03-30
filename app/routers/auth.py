f581429d1caab0c5e7c4857e70dd1c226161da91
Dikshant Gupta <guptadikshant99@gmail.com>
2023-03-30 23:08:04 +0530

    added jwt token creation logic

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas import UserLogin
-from app import models, utils
+from app import models, utils, outh2
 
 
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/login")
-def login(user_credentials:UserLogin, db: Session = Depends(get_db)):
+def login(user_credentials:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
 
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -21,5 +22,10 @@ def login(user_credentials:UserLogin, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Invalid Credentials")
     
+    access_token = outh2.create_access_token(data={"user_id": user.id})
+    
 
-    return {"token":"example token"}
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
